9daa98684470af5ada1b7eebf8c6bc87dcd534e0
stevenandz-glitch <stevenandz92491@gmail.com>
2025-12-14 13:06:27 -0800

    Drastically changed styling

--- rockpapsc.js
+++ rockpapsc.js
@@ -1,3 +1,17 @@
+const reset_button = document.querySelector("#reset");
+const enter_button = document.querySelector("#enter");
+const select_buttons = document.querySelector(".select__buttons");
+const tie_alert = document.querySelector(".is__tie");
+
+let user_input = document.querySelector("#user");
+let computer_input = document.querySelector("#computer");
+let score_input = document.querySelector("#score");
+
+let user_points = 0;
+let computer_points = 0;
+let rounds = 0;
+let game_ended = false;
+
 function RandomNumber() {
   return Math.floor(Math.random() * 3);
 }
@@ -27,55 +41,53 @@ function ValidMove(word) {
 function TypeWriter(element_id, text, delay_ms = 100) {
   const target_element = document.querySelector('#' + element_id);
   let char_index = 0;
+  target_element.value = null;
 
   function TypeCharacter() {
     if (char_index < text.length) {
       new Audio("sounds/type_sound.mp3").play();
       target_element.value += text.charAt(char_index++);
+      enter_button.disabled = true;
+      reset_button.disabled = true;
       setTimeout(TypeCharacter, delay_ms);
+    } else {
+      enter_button.disabled = false;
+      reset_button.disabled = false;
     }
   }
 
   TypeCharacter();
 }
 
-function WriteFinal(message) {
-  score_input.value = ' ';
-  TypeWriter("score", message, 100);
+function TrimValue(element) {
+  element.value = (
+    element.value.charAt(0).toUpperCase() + element.value.slice(1)).trim();
 }
 
-let user_points = 0;
-let computer_points = 0;
-let rounds = 0;
-let game_ended = false;
-
-let user_input = document.querySelector("#user");
-let computer_input = document.querySelector("#computer");
-let score_input = document.querySelector("#score");
-
-const reset_button = document.querySelector("#reset");
-const enter_button = document.querySelector("#enter");
-
-const select_buttons = document.querySelector(".select__buttons");
+function WriteFinal(message) {
+  TypeWriter("score", "Game Ended", 75);
+  tie_alert.textContent = message;
+  tie_alert.style.animation = "pull_down 0.5s forwards";
+  enter_button.disabled = true;
+}
 
 select_buttons.querySelectorAll("button").forEach((element)=>{
   element.addEventListener("click", ()=>{
     user_input.value = ' ';
-    user_input.value += element.textContent;
+    user_input.value += element.id;
   });
 });
 
 enter_button.addEventListener("click", ()=>{
   rounds++;
-  user_input.value = user_input.value.charAt(0).toUpperCase() + user_input.value.slice(1);
-  user_input.value = user_input.value.trim();
+  TrimValue(user_input);
 
   if (!ValidMove(user_input.value)) {
-    score_input.value = ' ';
     TypeWriter("score", "Invalid User Input", 75);
   } else {
     const computer_value = RandomMove(RandomNumber());
     const user_value = user_input.value;
+    tie_alert.style.animation = "pull_up 0.5s ease-in-out forwards";
 
     if (((user_value === "Paper") && (computer_value === "Rock")) ||
         ((user_value === "Scissors") && (computer_value === "Paper")) || 
@@ -83,45 +95,46 @@ enter_button.addEventListener("click", ()=>{
 
       user_points++;
     } else if (user_value === computer_value) {
-
-      alert("It is a tie");
+      tie_alert.style.display = "block";
+      tie_alert.style.animation = "pull_down 0.5s ease-in-out forwards";
     } else {
-
       computer_points++;
     }
 
     score_input.value = `User: ${user_points} || Computer: ${computer_points} || ${rounds}`;
-    computer_input.value = ' ';
     TypeWriter("computer", computer_value, 75);
 
-    if (rounds === 5) {
-      enter_button.setAttribute("diabled", true); 
+    if (rounds == 5) {
       game_ended = true;
+      enter_button.style.display = "none";
     }
   }
 
   if (game_ended) {
-    
     if (user_points > computer_points) {
-      WriteFinal("The User Won");
+      WriteFinal("Player Won");
     } else if (user_points < computer_points) {
-      WriteFinal("The Computer Won");
+      WriteFinal("Computer Won");
     } else {
       WriteFinal("No One Won");
     }
   }
+
 });
 
+
 reset_button.addEventListener("click", ()=>{
-  enter_button.removeAttribute("disabled");
+  enter_button.style.display = "block";
   game_ended = false;
   user_points = 0;
   computer_points = 0;
   rounds = 0;
-  score_input.value = ' ';
   TypeWriter("score", "Game Is Reset");
-  user_input.value = ' ';
-  computer_input.value = ' ';
+  user_input.value = null;
+  computer_input.value = null;
+  tie_alert.style.display = "none";
+  tie_alert.textContent = "TIED!";
+
 });
 
 
